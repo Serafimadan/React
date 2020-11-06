158dcb60fcc3ef31d2eefd2d023db8f27cb02068
serafima <svp1982@mail.ru>
2020-11-06 19:35:27 +0100

    Create react app

---
+++ week1/react-exercises/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HobbyList from './components/HobbyList';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world  
+      <HobbyList/>
+    </div>
+  );
+}
+
+export default App;
